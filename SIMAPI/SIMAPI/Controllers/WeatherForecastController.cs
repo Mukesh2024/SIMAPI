68738dcce0bf40d95306cb78d14fe950c0bbca4e
Dinesh Kumar <dinesh.kumar@rethinkfirst.com>
2025-03-24 13:53:57 +0530

    Tets

--- SIMAPI/SIMAPI/Controllers/WeatherForecastController.cs
+++ SIMAPI/SIMAPI/Controllers/WeatherForecastController.cs
@@ -21,6 +21,8 @@ namespace SIMAPI.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            Console.WriteLine("Test");
+
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
