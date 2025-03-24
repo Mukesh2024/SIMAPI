9d8413a329679ca05e783047e8b032cc7905f9f8
Dinesh Kumar <dinesh.kumar@rethinkfirst.com>
2025-03-24 15:15:18 +0530

    Working ChatGPT  Helper

--- SIMAPI/SIMAPI/Controllers/WeatherForecastController.cs
+++ SIMAPI/SIMAPI/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using SIMAPI.Helper;
 
 namespace SIMAPI.Controllers
 {
@@ -18,18 +19,29 @@ namespace SIMAPI.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
-        public IEnumerable<WeatherForecast> Get()
-        {
-            Console.WriteLine("Test");
+        //[HttpGet(Name = "GetWeatherForecast")]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    Console.WriteLine("Test");
 
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+        [HttpGet(Name = "GetChatGPTHelper")]
+        public async Task<object> GetChatGPTHelper()
+        {
+            var helper = new ChatGPTHelper();
+
+            var data  = await helper.SendRequest();
+
+            return data;
+
         }
     }
 }
